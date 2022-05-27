91c3c5c4c49fb0e64de49b7c31682bff7ffa15bf
LajinY10 <106390967+LajinY10@users.noreply.github.com>
2022-05-27 19:49:06 +0530

    Add files via upload

---
+++ bmi.php
@@ -0,0 +1,6 @@
+<?php
+$m=(float)readline("enter your height in meter: ");
+$h=$m*$m;
+$w=(float)readline("enter your weight: ");
+$bmi=$w/$h;
+print "your BMI is $bmi";
---
+++ salary.php
@@ -0,0 +1,7 @@
+<?php
+$name = readline('Hello ! what is your name ? ');
+$age=readline("hi ".$name."! how old are you ? ") ;
+echo("so your are $age.") ;
+$age=readline(" How much  do you make, .$name. ? ") ;
+echo("wow great !");
+?>
