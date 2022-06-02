0b2e91a5cdfce63ae221df7b7c73a486ba9112bb
LajinY10 <106390967+LajinY10@users.noreply.github.com>
2022-06-02 17:00:02 +0530

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,7 @@
+<?php
+$age=readline(" how old are you ? ") ;
+$weight=(float)readline(" how much weight are you ? ") ;
+$height=(float)readline(" how tall are you ? ") ;
+print("so your age is $age and your height is $height and your weight is $weight !")
+?>
+
---
+++ 6.php
@@ -0,0 +1,6 @@
+<?php
+$f=(float)readline("first number : ");
+$s=(float)readline("second number : ");
+$t=(float)readline("third number : ");
+$sum=($f+$s+$t)/2;
+echo $sum;
