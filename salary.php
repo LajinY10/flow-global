9f6294a8dd344b371588e063752b2694fa4c0009
LajinY10 <106390967+LajinY10@users.noreply.github.com>
2022-06-02 16:59:35 +0530

    Delete salary.php

--- salary.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = readline('Hello ! what is your name ? ');
-$age=readline("hi ".$name."! how old are you ? ") ;
-echo("so your are $age.") ;
-$age=readline(" How much  do you make, .$name. ? ") ;
-echo("wow great !");
-?>
